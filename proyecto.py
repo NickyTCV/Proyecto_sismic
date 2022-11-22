19e7cf890a2c2b9e606769a538e2b72e9cefc829
Nicole Gutierrez <117460418+NickyTCV@users.noreply.github.com>
2022-11-21 20:54:11 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -13,7 +13,7 @@ import streamlit as st
 import plotly.express as px
 import plotly.figure_factory as ff
 import scipy
-------------------------------
+
 st.title("Catálogo sísmico 1960-2021")
 df_cat = pd.read_excel(r'https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.xlsx', header= 0) 
 st.write(df_cat)
