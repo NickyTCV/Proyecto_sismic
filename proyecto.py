4128015ebd55a34c2011fcb728c37adb001cc891
Nicole Gutierrez <117460418+NickyTCV@users.noreply.github.com>
2022-11-21 20:44:04 -0500

    Create proyecto.py

---
+++ proyecto.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import gdown
+
+import pip
+pip.main(["install", "openpyxl"])
+pip.main(["install", "pandas"])
+import plotly.figure_factory as ff
+pip.main(["install", "matplotlib"])
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.figure_factory as ff
+import scipy
+
+df_cat = pd.read_excel(r'https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.xlsx', header= 0) 
+st.write(df_cat)
