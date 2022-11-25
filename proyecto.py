88e4aff4d45c46794384b30aa2bae6dc578926ad
heidi1904 <117460221+heidi1904@users.noreply.github.com>
2022-11-25 16:54:23 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,27 +1,8 @@
-#$ pip install streamlit --upgrade 
 import streamlit as st
 import pandas as pd
 import numpy as np
 import gdown
-import urllib.request
-from streamlit_option_menu import option_menu
 
-with st.sidebar: 
-    st.markdown("###")
-    st.sidebar.header('Programación Avanzada')
-    selected = option_menu(
-        menu_title = 'Menú',
-        options = ['Inicio', 'Reporte','Equipo'],
-        icons = ['house', 'book', 'book','people'],
-        menu_icon='cast',
-        default_index = 0,
-    )
-
-if selected == 'Inicio':
-  st.markdown("<h1 style ='text-align: center'>Titulo:</h1>", unsafe_allow_html=True)
-  st.markdown("---")
-  st.header("Dataset")
-  
 import pip
 pip.main(["install", "openpyxl"])
 pip.main(["install", "pandas"])
